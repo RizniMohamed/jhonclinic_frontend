be4274d628ae758442bef9fb133cb1ecc88a634c
Rizni <mnriznimohamed@gmail.com>
2022-08-08 03:13:23 +0530

    protected routes

--- src/routes/Views.js
+++ src/routes/Views.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom"
 import Dashbaord from "../pages/admin/Dashbaord";
 import Admin from "../pages/admin";
 import Login from "../pages/Login";
@@ -6,6 +6,36 @@ import NotFound from "../pages/NotFound";
 import Patients from "../pages/admin/Patients";
 import Records from "../pages/admin/Records";
 
+import { Box } from '@mui/system'
+import SidePanel from '../components/SidePanel'
+import Header from "../components/Header"
+import { Toolbar } from '@mui/material'
+import { store } from "../store";
+
+
+const ProtectedRoute = ({ children, }) => {
+    const auth = store.getState().auth
+    const location = useLocation()
+    const path = location.pathname.split('/').filter(x => x)
+
+    if (!auth.status) {
+        if (location.pathname.split('/').filter(x => x).length === 1)
+            return <Navigate to={'/'} replace />;
+        path.pop()
+        return <Navigate to={`/${path.join('/')}`} replace />;
+    }
+
+    return children ? children : (
+        <Box p={1} >
+            <Header />
+            <Toolbar />
+            <Box display="flex">
+                <SidePanel />
+                <Outlet />
+            </Box>
+        </Box>
+    )
+};
 
 function Views() {
     return (
@@ -13,10 +43,10 @@ function Views() {
 
             <Route index element={<Login />} />
 
-            <Route path="admin" element={< Admin />} >
-                <Route index element={<Dashbaord/>}/>
-                <Route path="patients" element={<Patients/>}/>
-                <Route path="patients/:userID/records" element={<Records/>}/>
+            <Route path="admin" element={< ProtectedRoute />} >
+                <Route index element={<Dashbaord />} />
+                <Route path="patients" element={<Patients />} />
+                <Route path="patients/:userID/records" element={<Records />} />
             </Route>
 
             <Route path="*" element={<NotFound />} />
