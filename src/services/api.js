92385055b78368d6fe128898ed223abb37df7ce0
Rizni <mnriznimohamed@gmail.com>
2022-08-13 03:08:47 +0530

    base url local

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://127.0.0.1:8000/', });
+const API = axios.create({ baseURL: 'http://127.0.0.1:8000/', });
 
 export default API;
